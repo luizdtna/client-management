23729ebbcbf3ea78349319525575c540704d1954
Luiz Araujo <luiz.araujodtna@gmail.com>
2019-08-22 18:04:13 -0300

    Configure the app

--- Project_Manage_Client/settings.py
+++ Project_Manage_Client/settings.py
@@ -11,21 +11,24 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config #segurança: para não subir as variávies criticas
+from dj_database_url import parse as dburl #segurança: esconte informações senssiveis da url do BD
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xsljjh#xzcux=y@bojfu1t-ll-)whx1vh7e$svt0hy)yg)&4*r'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['management-clients.herokuapp.com']
 
 
 # Application definition
@@ -82,7 +85,11 @@ WSGI_APPLICATION = 'Project_Manage_Client.wsgi.application'
 #    }
 #}
 
-DATABASES = {
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES = { 'default': config('DATABASE_URL', default=default_dburl, cast=dburl), }
+
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'mybanc',
@@ -93,7 +100,7 @@ DATABASES = {
     }
 }
 
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -132,6 +139,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = 'media'
