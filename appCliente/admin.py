9bea47fe55b7ef1ee03aeab61dbaa2232bfcf9f3
Luiz Araujo <luiz.araujodtna@gmail.com>
2019-08-25 16:46:07 -0300

    database remote

--- appCliente/admin.py
+++ appCliente/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Pessoa
+from .models import Pessoa, Document, Sale, Product
 
 # Register your models here.
 admin.site.register(Pessoa)
+admin.site.register(Document)
+admin.site.register(Sale)
+admin.site.register(Product)
--- appCliente/models.py
+++ appCliente/models.py
@@ -2,6 +2,12 @@ from django.db import models
 
 # Create your models here.
 
+class Document(models.Model):
+    num_doc = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.num_doc
+
 class Pessoa(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -9,5 +15,22 @@ class Pessoa(models.Model):
     salary = models.DecimalField(max_digits=10,decimal_places=2)
     bio = models.TextField(max_length=2000)
     photo = models.ImageField(upload_to ='clients_photos',null = True , blank=True)
+    doc = models.OneToOneField(Document, null=True,blank=True,on_delete=models.CASCADE)
     def __str__(self):
         return self.first_name +" "+ self.last_name
+
+class Product(models.Model):
+    description = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    def __str__(self):
+        return self.description
+
+class Sale(models.Model):
+    number = models.CharField(max_length=10)
+    value = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    taxes = models.DecimalField(max_digits=10, decimal_places=2)
+    person = models.ForeignKey(Pessoa, null=True,blank=True,on_delete=models.PROTECT)#se Pessoa ja tiver venda, não permite deletar
+    products = models.ManyToManyField(Product,blank=True)
+    def __str__(self):
+        return self.number
